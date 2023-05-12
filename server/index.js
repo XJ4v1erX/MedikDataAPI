bb0230ba3e05df4f05c948396a17b110e0335cd7
Javier Alejandro Ramírez Heredia <javieralejandroramirez69@gmail.com>
2023-05-11 21:50:32 -0600

    update
    update

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(cors())
 //ROUTES//
 
 app.use("/auth", require("./routes/jwtAuth"));
+app.use("/dashboard", require("./routes/dashboard"));
 
 app.listen(5000, () => {
     console.log('server has started on port 5000');
